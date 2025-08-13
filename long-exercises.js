afbf25bbae3494ded989229c780b46fdfabf27ef
Caleb TUYISINGIZE <calebmevis11@gmail.com>
2025-08-13 18:36:20 +0200

    EXERCISE 7 on Phone number validator

--- long-exercises.js
+++ long-exercises.js
@@ -121,4 +121,13 @@ const inputObject = {
  const transformObject = transformKeys(inputObject, transformKey);
 console.log(transformObject);
 
+const validatePhoneNumber = (number)=>{
+  const pattern = /^(\d{10}|\d{3}[- ]\d{3}[- ]\d{4})$/;
+  return pattern.test(number)
+}
+console.log(validatePhoneNumber("1234567890"));
+console.log(validatePhoneNumber("123-456-7890"));
+console.log(validatePhoneNumber("123 456 7890"));
+console.log(validatePhoneNumber("123-45-67890")); 
+console.log(validatePhoneNumber("1234-567890"));
 
