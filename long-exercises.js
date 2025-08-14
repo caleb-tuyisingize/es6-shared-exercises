2eccff544a7512d00a3711975482052db4772563
Caleb TUYISINGIZE <calebmevis11@gmail.com>
2025-08-14 08:19:47 +0200

    Revising on array methods from scratch

--- long-exercises.js
+++ long-exercises.js
@@ -130,4 +130,3 @@ console.log(validatePhoneNumber("123-456-7890"));
 console.log(validatePhoneNumber("123 456 7890"));
 console.log(validatePhoneNumber("123-45-67890")); 
 console.log(validatePhoneNumber("1234-567890"));
-
