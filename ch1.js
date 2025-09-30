f4349a6550d5ad24b972487cd927f02defbe27e4
Caleb TUYISINGIZE <calebmevis11@gmail.com>
2025-09-30 20:41:01 +0200

    deep clone with spreed

---
+++ ch1.js
@@ -0,0 +1,27 @@
+const clone = (obj)=>{
+     if (obj === null || typeof obj !== "object") return obj;
+const cloned = {...Object.fromEntries(Object.entries(obj).map(([k, j])=> [k, clone(j)]))};
+    return cloned;
+ 
+}
+
+const person = {
+    name: "Caleb",
+    age: 21,
+    level: 2,
+    school: {
+        sname:"Collegio",
+        location: "Huye",
+        number: 23
+
+    }
+
+}
+const newPerson = clone(person);
+console.log(newPerson.name);
+
+// Structure clone
+
+
+
+// json.parse(json.stringfy)
