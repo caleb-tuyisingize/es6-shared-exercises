76185089f2cb8daa8e7fcde3f21011e2b6a78253
Caleb TUYISINGIZE <calebmevis11@gmail.com>
2025-10-01 10:08:34 +0200

    .

---
+++ inhert.js
@@ -0,0 +1,20 @@
+class Person{
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+    greet(){
+        console.log(`Hello my name is ${this.name}, and I am ${this.age} years old`);
+    }
+}
+
+class Student extends Person{
+    constructor(name, age, course){
+        super(name,age);
+        this.course = course;
+    }
+    study(){
+
+    }
+}
+console.log()
